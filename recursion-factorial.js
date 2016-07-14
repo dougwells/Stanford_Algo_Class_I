28f551c4c6552dc52329a78aadc4b5a3e3b912a5
dougwells <dougwells@gmail.com>
2016-07-14 11:49:42 -0600

    Fibonacci2 WIP

--- recursion-factorial.js
+++ recursion-factorial.js
@@ -1,16 +1,31 @@
 "use strict"
-let recursionTree = {};
-let sum = 1;
-
+let product, current = 1;
+let previous, sum = 0
 
 let factorial = (n) => {
   if(n>0) {
-    sum = sum*n;
+    product = product*n;
     factorial(n-1);
   }else{
-    return console.dir(sum);
-
+    return product;
   }
 }
 
+let factorialShort = (n) => {
+  if (n<2) {return 1}
+    return n*factorialShort(n-1)
+}
+
+let fibonacci = (n, max) => {
+  if (n=1){previous = 1; return previous}
+  if (n<max){
+
+    return console.log(sum);
+  } else{
+    console.log('end');
+  }
+  }
+
 factorial(7);
+console.log('factorialShort= ', factorialShort(7));
+fibonacci(0,7);
---
+++ recursion-fibonacci.js
@@ -0,0 +1,22 @@
+"use strict"
+let tree = {};
+let previous = 0;
+let current = 0;
+let sum = 1;
+
+
+let fibonacci = (n, max) => {
+  if(n<=max) {
+    console.log(sum);
+    previous = sum;
+    sum = previous + current;
+    current = previous
+
+    fibonacci(n+1, max);
+  }else{
+    return console.log(sum);
+
+  }
+}
+
+fibonacci(1,20);
