75cc98f2af06cb275fefb965504ba5bcde1ceacb
dougwells <dougwells@gmail.com>
2016-07-23 12:21:28 -0600

    Wk2 Quicksort.  Algorithm working. Next Test cases

--- Tests/Week2/1quicksort.js
+++ Tests/Week2/1quicksort.js
@@ -6,14 +6,13 @@ var testArr = [3,8,2,5,1,4,7,6];
 let partition = (arr, startIndex, endIndex) => {
   let l = startIndex || 0;
   let r = endIndex || (arr.length-1);
-  console.log('start', arr, l, r);
-  if((r-l)<=1){return i;}
-
   let pivot = arr[l];
   let i = l+1
+  console.log('partition called', pivot,l, r, arr);
+
 
   for (var j=l+1; j<=r; j++){
-    console.log("pivot, j, i, arr", pivot, j, i, arr);
+    // console.log("pivot, i, j, arr", pivot, i, j, arr);
     if(arr[j]<pivot){
       arr = swap (i, j, arr)
       i++;
@@ -21,7 +20,8 @@ let partition = (arr, startIndex, endIndex) => {
   }
   //put pivot in correct location
   swap (l, i-1, arr);
-  console.log("pivot, j, i, arr", pivot, j, i, arr);
+  console.log("move pivot, i, j, arr", pivot, i, j, arr);
+
   return i;
 }
 
@@ -35,17 +35,19 @@ let swap = (leftIndex, rightIndex, array) =>{
 }
 
 var quickSort = function(array, p, r) {
-  if(p < r){
+  console.log("quicksort called", p, r, array);
+  if(r-p>1){
     var q = partition(array, p, r);
     quickSort(array, p, q - 1);
-    quickSort(array, q + 1, r);
+    quickSort(array, q, r);
+  }else{
+    return array;
   }
-  return array;
 }
 
 var array = [9, 7, 5, 11, 12, 2, 14, 3, 10, 4, 6];
-var q = partition(array, 0, array.length - 1);
-console.log(q);
-// 4
-console.log(quickSort(array, 0, array.length - 1));
+// var q = partition(array, 0, array.length - 1);
+// console.log("Partition done", q);
+// // 4
+quickSort(array, 0, (array.length - 1));
 // [ 2, 3, 4, 5, 6, 7, 9, 10, 11, 12, 14 ]
